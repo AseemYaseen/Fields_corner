2ba64e3c621f309c8474c8d0db48f810cdf648ee
unknown <shaima.alshlouh@gmail.com>
2023-01-30 09:39:52 +0200

    edit

--- resources/views/puplicUser/userprofile.blade.php
+++ resources/views/puplicUser/userprofile.blade.php
@@ -32,7 +32,6 @@ User Profile
                   </div>
                 </div>
               </div>
-
             </div>
             <div class="col-md-8">
               <div class="card mb-3">
@@ -56,24 +55,6 @@ User Profile
                   </div>
                   <hr>
                   <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Phone</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      (239) 816-9029
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Address</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                      Bay Area, San Francisco, CA
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
                     <div class="col-sm-12">
                       <a class="btn btn-primary " target="__blank" href="{{ url('Edit') }}">Edit</a>
                     </div>
@@ -82,7 +63,6 @@ User Profile
               </div>
             </div>
           </div>
-
         </div>
     </div>
 
