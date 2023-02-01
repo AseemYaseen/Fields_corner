b915c7b079d0c081e4bf5b15c5d32d99b5a0f823
unknown <shaima.alshlouh@gmail.com>
2023-02-01 21:38:54 +0200

    edit2

--- resources/views/puplicUser/layout/navbar.blade.php
+++ resources/views/puplicUser/layout/navbar.blade.php
@@ -15,7 +15,7 @@
 
   {{-- <div id="menu-bar" class="fas fa-bars "></div> --}}
 
-  <a class="logo" href="{{ route('puplicUser.welcome') }}"><img src="/img/logo.png" class="hires" width="220" height="80" alt="Free Bootstrap Template by uicookies.com"></a>
+  <a class="logo" href="{{ route('puplicUser.welcome') }}"><img src="/img/logo.png" class="hires" width="250" height="80" alt="Free Bootstrap Template by uicookies.com"></a>
 
   {{-- <a href="#" class="logo"><span>T</span>ravel</a> --}}
 
